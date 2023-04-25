d393b74827acd7a1182b5331ed820da9642c4104
Gerd Zellweger <mail@gerdzellweger.com>
2023-04-25 16:22:41 -0700

    Move columns up.

--- web-ui/src/components/table/ErrorOverlay.tsx
+++ web-ui/src/components/table/ErrorOverlay.tsx
@@ -1,7 +1,7 @@
 // The error message that is shown instead of the EntityTable if we failed to
 // load data for some reason (e.g. network error).
 
-import { Alert, AlertTitle, Link } from '@mui/material'
+import { Alert, AlertTitle } from '@mui/material'
 
 export interface ErrorProps {
   isError: boolean
